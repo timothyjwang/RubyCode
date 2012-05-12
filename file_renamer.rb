0ea4f79e5ce279dfc52a8323b46267d3489bc459
Anthony Severini <FluffyCode@gmail.com>
2012-05-11 19:02:30 -0700

    changes to file_renamer.rb

--- file_renamer.rb
+++ file_renamer.rb
@@ -1,42 +1,31 @@
-# Version 1.0
-
-# The program takes an input InThisKindOfFormat, and will turn it into a string in_this_kind_of_format
+# Version 1.1
+# The program takes an input InThisKindOfFormat, and will turn it into a string in_this_kind_of_format.
 
 # The user is prompted for an input - the input is saved in a variable:
 puts "Please input the file to be renamed:"
-rename_me = gets.chomp.to_s
+rename_me = gets.chomp
+
+CAPTURE_CAPITAL_REGEXP = /
+(            (?# Start a capture group with an open parentheses)
+[A-Z]        (?# Match any single character ranging from A to Z)
+)            (?# Close the capture group so that the character will be captured)
+/x
 
 def rename_method str_to_rename
-	# variable x, indexing for the file name:
-	x = 0
-	# variable storing the file name:
-	file_name_prog = ""
+	new_filename = str_to_rename.gsub CAPTURE_CAPITAL_REGEXP do |matched_capital|
+	matched_capital.prepend("_").downcase
+end
 
-	def has_uppercase_letters this_capital
-		(this_capital =~ /[A-Z]/) != nil
-	end
+new_filename.gsub! /^_/, ""
 
-	while x < str_to_rename.length
-		if x == 0
-			file_name_prog += str_to_rename[x]
-		elsif has_uppercase_letters(str_to_rename[x]) == true
-			file_name_prog += str_to_rename[x].gsub(/([A-Z])/, ('_' + str_to_rename[x]))
-		else
-			file_name_prog += str_to_rename[x]
-		end
-		x += 1
-	end
-
-	file_name_prog.downcase!
-
-	puts "Is '#{file_name_prog}' the naming convention you were looking for?"
-	if gets.chomp.downcase == "yes"
-		File.rename(str_to_rename, file_name_prog) # Renaming the file
-		puts "Alrighty - the file name is now #{file_name_prog}."
-	else
-		puts "No changes have been made."
-		exit 0
-	end
+puts "Is '#{new_filename}' the naming convention you were looking for?"
+if gets.chomp.downcase == "yes"
+	File.rename(str_to_rename, new_filename) # Renaming the file
+	puts "Alrighty - the file name is now #{new_filename}."
+else
+	puts "No changes have been made."
+	exit 0
+end
 end
 
 rename_method rename_me
