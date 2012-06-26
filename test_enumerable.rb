4d9bc440c118be03019bdbff5e2c5ce3931929a0
Anthony Severini <FluffyCode@gmail.com>
2012-06-26 10:34:57 -0700

    Play around with .count

--- test_enumerable.rb
+++ test_enumerable.rb
@@ -80,3 +80,21 @@ puts "There is an Ace." if has_an_ace.any? { |word| word.include?("Ace") } == tr
 
 doesnt_have_an_ace = ["Four of Clubs", "Ten of Diamonds", "Three of Hearts"]
 puts "There are no Aces." if doesnt_have_an_ace.any? { |word| word.include?("Ace") } == false
+
+puts
+
+
+
+# Messing around with .count
+hand_of_cards = ["Two of Hearts", "Ace of Spades"]
+puts "There are #{hand_of_cards.count} cards in the hand."
+
+hand_of_cards.push("Walrus of Cheese")
+puts "We just added 'Walrus of Chese' to the hand. The hand now contains:"
+puts hand_of_cards.join(", ")
+puts "There are now #{hand_of_cards.count} cards in the hand."
+
+# There is a "Two of Hearts in the hand:"
+puts hand_of_cards.count("Two of Hearts")
+# But there is nothing with "Two" in the hand:
+puts hand_of_cards.count("Two")
