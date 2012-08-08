adc5fa3e6f0b73cd4e5e18f5be51133c229e8cc1
Anthony Severini <FluffyCode@gmail.com>
2012-08-08 11:05:07 -0700

    Stage 3 (final); find way to resolve transfer of cards in go_fish.

--- test_do_each.rb
+++ test_do_each.rb
@@ -37,4 +37,27 @@ end
 
 puts "Fluffy kittens: #{fluffy_kittens.join(", ")}."
 puts "Stuff about fluffy kittens: #{stuff_about_fluffy_kittens.join(", ")}."
-puts increments
+puts "Increments: #{increments}."
+
+puts
+
+# Third round...
+
+fluffy_kittens = ["cute", "adorable", "fluffy"]
+stuff_about_fluffy_kittens = []
+x = 0
+
+puts "Fluffy kittens: #{fluffy_kittens.join(", ")}."
+puts "Stuff about fluffy kittens: #{stuff_about_fluffy_kittens.join(", ")}."
+puts
+
+until fluffy_kittens[x] == nil
+	if fluffy_kittens[x].include?("e")
+		stuff_about_fluffy_kittens << fluffy_kittens.delete(fluffy_kittens[x])
+	else
+		x += 1
+	end
+end
+
+puts "Fluffy kittens: #{fluffy_kittens.join(", ")}."
+puts "Stuff about fluffy kittens: #{stuff_about_fluffy_kittens.join(", ")}."
