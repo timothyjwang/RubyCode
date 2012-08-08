8cfc942d2163da2b30840db37b8f4f4c572bbcac
Anthony Severini <FluffyCode@gmail.com>
2012-08-07 17:10:21 -0700

    Test do-each; stage 2.

--- test_do_each.rb
+++ test_do_each.rb
@@ -3,11 +3,38 @@
 fluffy_kittens = ["cute", "adorable", "fluffy"]
 stuff_about_fluffy_kittens = []
 
+puts "Fluffy kittens: #{fluffy_kittens.join(", ")}."
+puts "Stuff about fluffy kittens: #{stuff_about_fluffy_kittens.join(", ")}."
+puts
+
 fluffy_kittens.each do |attribute|
 	if attribute.include?("e")
 		stuff_about_fluffy_kittens << fluffy_kittens.delete(attribute)
 	end
 end
 
-puts fluffy_kittens.join(", ")
-puts stuff_about_fluffy_kittens.join(", ")
+puts "Fluffy kittens: #{fluffy_kittens.join(", ")}."
+puts "Stuff about fluffy kittens: #{stuff_about_fluffy_kittens.join(", ")}."
+
+puts
+
+# Second round...
+
+fluffy_kittens = ["cute", "adorable", "fluffy"]
+stuff_about_fluffy_kittens = []
+increments = 0
+
+puts "Fluffy kittens: #{fluffy_kittens.join(", ")}."
+puts "Stuff about fluffy kittens: #{stuff_about_fluffy_kittens.join(", ")}."
+puts
+
+fluffy_kittens.each do |attribute|
+	if attribute.include?("e")
+		stuff_about_fluffy_kittens << fluffy_kittens.delete(attribute)
+		increments += 1
+	end
+end
+
+puts "Fluffy kittens: #{fluffy_kittens.join(", ")}."
+puts "Stuff about fluffy kittens: #{stuff_about_fluffy_kittens.join(", ")}."
+puts increments
