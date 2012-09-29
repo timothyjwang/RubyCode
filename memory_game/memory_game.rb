97e2b467a63570ea0e3d7d3d19c0a8cb77b52ddc
Anthony Severini <FluffyCode@gmail.com>
2012-09-29 11:27:13 -0700

    Remove old file.

--- memory_game/memory_game.rb
+++
@@ -1,3 +0,0 @@
-cards = ["Red Circle", "Red Circle", "Red Square", "Red Square", "Red Triangle", "Red Triangle",
-		"Green Circle", "Green Circle", "Green Square", "Green Square", "Green Triangle", "Green Triangle"
-		"Blue Circle", "Blue Circle", "Blue Square", "Blue Square", "Blue Triangle", "Blue Triangle"]
