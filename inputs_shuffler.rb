298f2de5eaba2c66a04e01131b0677707adcc2cd
Anthony Severini <FluffyCode@gmail.com>
2012-12-20 22:22:51 -0800

    Refactor inputs_shuffler.

--- inputs_shuffler.rb
+++ inputs_shuffler.rb
@@ -1,39 +1,56 @@
-# Version 1.0
-# Similar to inputs_alphabetizer.rb - takes inputs, and shuffles them into a random order.
+# Version 2.0
+# Ofshoot of inputs_alphabetizer.rb - takes inputs, and shuffles them into a random order.
 
-puts "Add some stuff to shuffle.  When you are finished, enter a blank space."
-shuffle_me = []
-big_ass_prompt = "How would you like your list?\n1: On individual lines?\n...or...\n2: All one one line, separated by commas?"
-run_me = 1
+class Inputs_Shuffler
+	def initialize
+		@shuffle_me = []
+		@big_ass_prompt = "How would you like your list?\n1: On individual lines?\n...or...\n2: All one one line, separated by commas?"
+	end
 
-while run_me == 1
-	user_input = gets.chomp.to_s
-	if user_input != ""
-		shuffle_me.push user_input
-	else
-		puts "Is that all?"
-		if gets.chomp.to_s.downcase == "yes"
-			shuffle_me.sort_by!{rand}
-			
-			puts "All done shuffling."
-			puts big_ass_prompt
-			user_sorting_choice = gets.chomp.to_s
-			
-			until user_sorting_choice == "1" || user_sorting_choice == "2"
-				puts "" # Blank space
-				puts "Input error: expecting either a 1 or a 2."
-				puts big_ass_prompt
-				user_sorting_choice = gets.chomp.to_s
-			end
-			if user_sorting_choice == "1"
-				puts shuffle_me
-				exit 0
-			elsif user_sorting_choice == "2"
-				puts shuffle_me.join(', ')
-				exit 0
-			end
+	def get_shufflin
+		puts "#{@big_ass_prompt}"
+		
+		user_sorting_choice = gets.chomp.to_i
+		if user_sorting_choice == 1
+			puts @shuffle_me
+			exit 0
+		elsif user_sorting_choice == 2
+			puts "#{@shuffle_me.join(', ')}"
+			exit 0
 		else
-			# Anything but "yes" will have the while loop continue to re-run
+			puts "Input error: expecting either a 1 or a 2."
+			get_shufflin
 		end
 	end
+
+	def add_to_array
+		puts "Add some stuff to shuffle.  When you are finished, enter a blank space."
+		add_stuff
+	end
+
+	def end_adding_stuff
+		puts "Are you finished adding stuff? (y/n)"
+	
+		user_response = gets.chomp.downcase
+		if user_response == "y"
+			@shuffle_me.shuffle!
+			get_shufflin
+		else
+			add_to_array
+		end
+	end
+
+	def add_stuff
+		this_thing = gets.chomp
+
+		if this_thing != ""
+			@shuffle_me << this_thing
+			add_stuff
+		else
+			end_adding_stuff
+		end
+	end
+
 end
+
+Inputs_Shuffler.new.add_to_array
