f478436a8726ffea576fc9cf0860a30a9a8d990e
Anthony Severini <FluffyCode@gmail.com>
2012-12-20 20:47:23 -0800

    Update version number - prep for refactoring.

--- inputs_shuffler.rb
+++ inputs_shuffler.rb
@@ -1,4 +1,4 @@
-# Version 0.1
+# Version 1.0
 # Similar to inputs_alphabetizer.rb - takes inputs, and shuffles them into a random order.
 
 puts "Add some stuff to shuffle.  When you are finished, enter a blank space."
