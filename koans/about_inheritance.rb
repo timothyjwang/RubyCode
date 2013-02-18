fa4a3382947c55bc92be61375e2bc3ec11c62d7b
Anthony Severini <FluffyCode@gmail.com>
2013-02-18 10:40:57 -0800

    Update progress: 216/280

--- koans/about_inheritance.rb
+++ koans/about_inheritance.rb
@@ -24,11 +24,11 @@ class AboutInheritance < EdgeCase::Koan
   end
 
   def test_subclasses_have_the_parent_as_an_ancestor
-    assert_equal __, Chihuahua.ancestors.include?(Dog)
+    assert_equal true, Chihuahua.ancestors.include?(Dog)
   end
 
   def test_all_classes_ultimately_inherit_from_object
-    assert_equal __, Chihuahua.ancestors.include?(Object)
+    assert_equal true, Chihuahua.ancestors.include?(Object)
   end
 
   def test_subclasses_inherit_behavior_from_parent_class
