415e425f182f03c08eed768468e5ac5df43d6b37
Anthony Severini <FluffyCode@gmail.com>
2013-02-18 20:08:55 -0800

    Update progress: level 1 complete

--- rubywarrior/fluffy-beginner/player.rb
+++ rubywarrior/fluffy-beginner/player.rb
@@ -1,5 +1,11 @@
+# RubyWarrior - Level 1
 class Player
-  def play_turn(warrior)
-    # add your code here
-  end
+	def play_turn(warrior)
+	
+		def walk(warrior)
+			warrior.walk!
+		end
+
+		walk(warrior)
+	end
 end
