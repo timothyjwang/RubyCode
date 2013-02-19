a42281a79ec29369dd83ab4e20efad9e1d7bdc8b
Anthony Severini <FluffyCode@gmail.com>
2013-02-18 20:14:51 -0800

    Update progress: level 2 complete

--- rubywarrior/fluffy-beginner/player.rb
+++ rubywarrior/fluffy-beginner/player.rb
@@ -1,4 +1,18 @@
 # RubyWarrior - Level 1
+# class Player
+# 	def play_turn(warrior)
+	
+# 		def walk(warrior)
+# 			warrior.walk!
+# 		end
+
+# 		walk(warrior)
+# 	end
+# end
+
+
+
+# RubyWarrior - Level 2
 class Player
 	def play_turn(warrior)
 	
@@ -6,6 +20,19 @@ class Player
 			warrior.walk!
 		end
 
-		walk(warrior)
+		def feel(warrior)
+			warrior.feel.empty?
+		end
+
+		def attack(warrior)
+			warrior.attack!
+		end
+
+		if feel(warrior)
+			walk(warrior)
+		else
+			attack(warrior)
+		end
+
 	end
 end
